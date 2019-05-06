23155d44ee06608b5e625624de451d7e0b675721
marcio-nunes <marcionlinerj@gmail.com>
2019-05-06 00:12:59 -0300

    ajuste na função test_qtd_de_spam()

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -8,21 +8,23 @@ from libpythonpro.spam.modelos import Usuario
 @pytest.mark.parametrize(
     'usuarios',
     [
-        Usuario(nome='Marcio', email='marcio@nunes.com'),
-        Usuario(nome='Luciane', email='luciane@oliveira.com')
-    ],
-    [
-        Usuario(nome='Marcio', email='marcio@nunes.com')
+        [
+            Usuario(nome='Marcio', email='marcio@nunes.com'),
+            Usuario(nome='Luciane', email='luciane@oliveira.com')
+        ],
+        [
+            Usuario(nome='Marcio', email='marcio@nunes.com')
+        ]
     ]
 )
-def test_qde_de_spam(sessao, usuarios):
+def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-        enviador = Enviador()
-        enviador_de_spam = EnviadorDeSpam(sessao, enviador)
-        enviador_de_spam.enviar_emails(
-            'marcio@nunes.com',
-            'Curso Python Pro',
-            'confira os módulos fantásticos'
-        )
-    assert len(usuarios) == enviador.qtde_email_enviados
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'marcio@nunes.com',
+        'Curso Python Pro',
+        'confira os módulos fantásticos'
+    )
+    assert len(usuarios) == enviador.qtd_emails_enviados
