a295cb73a535c26b0921e6cde597e90b84005ea5
marcio-nunes <marcionlinerj@gmail.com>
2019-05-08 00:35:33 -0300

    aplicando unittest.mock nos testes

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -5,15 +7,15 @@ from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 
 
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.parametros_de_envio = None
-        self.qtd_emails_enviados = 0
+# class EnviadorMock(Enviador):
+#     def __init__(self):
+#         super().__init__()
+#         self.parametros_de_envio = None
+#         self.qtd_emails_enviados = 0
 
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_emails_enviados += 1
+#     def enviar(self, remetente, destinatario, assunto, corpo):
+#         self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+#         self.qtd_emails_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -31,27 +33,27 @@ class EnviadorMock(Enviador):
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'marcio@nunes.com',
         'Curso Python Pro',
         'confira os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtd_emails_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Marcio', email='marcio@nunes.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'luciane@oliveira.com',
         'Curso Python Pro',
         'confira os módulos fantásticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'luciane@oliveira.com',
         'marcio@nunes.com',
         'Curso Python Pro',
