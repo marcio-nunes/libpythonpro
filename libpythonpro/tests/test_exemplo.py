4d0ed782f21ddc162327e3b17903d59f69066a0b
marcio-nunes <marcionlinerj@gmail.com>
2019-05-07 23:48:08 -0300

    aplicando injeção de dependência e mockando testes

--- libpythonpro/tests/test_exemplo.py
+++ libpythonpro/tests/test_exemplo.py
@@ -1,2 +1,7 @@
-def test_int():
-    assert 1 == 1
+import requests
+
+
+def test_conexao_github():
+    resp = requests.get('https://api.github.com/users/marcio-nunes')
+    assert resp.status_code == 200
+
--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -5,6 +5,17 @@ from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 
 
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.parametros_de_envio = None
+        self.qtd_emails_enviados = 0
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_emails_enviados += 1
+
+
 @pytest.mark.parametrize(
     'usuarios',
     [
@@ -20,7 +31,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'marcio@nunes.com',
@@ -28,3 +39,21 @@ def test_qtd_de_spam(sessao, usuarios):
         'confira os módulos fantásticos'
     )
     assert len(usuarios) == enviador.qtd_emails_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Marcio', email='marcio@nunes.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'luciane@oliveira.com',
+        'Curso Python Pro',
+        'confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'luciane@oliveira.com',
+        'marcio@nunes.com',
+        'Curso Python Pro',
+        'confira os módulos fantásticos'
+    )
