ef26a71c4686934a25ed1dc6c9f557f09d6aaba4
marcio-nunes <marcionlinerj@gmail.com>
2019-05-08 02:41:28 -0300

    isolando testes em github_api.py

---
+++ libpythonpro/tests/test_spam/test_buscar_avatar.py
@@ -0,0 +1,29 @@
+from unittest.mock import Mock
+
+import pytest
+
+from libpythonpro import github_api
+
+
+@pytest.fixture
+def avatar_url():
+    resp_mock = Mock()
+    url = 'https://avatars2.githubusercontent.com/u/45412154?v=4'
+    resp_mock.json.return_value = {
+        'login': 'marcio-nunes',
+        'avatar_url': url
+    }
+    get_original = github_api.requests.get
+    github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('marcio-nunes')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzon')
+    assert 'https://avatars3.githubusercontent.com/u/3457115?v=4' == url
